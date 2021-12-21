4526c62a99d809fbaf148cb8a38eab90c400981c
Jurgen Louvris <jurgen.louvris@student.rocva.nl>
2021-12-21 11:00:31 +0100

    Aanpassingen

---
+++ html/script.js
@@ -0,0 +1,10 @@
+function Test() {
+    document.getElementById("blok").style.display = "block";
+}
+
+function pic() {
+    document.getElementById("pic").style.display = "block";
+}
+function code() {
+    document.getElementById("code").style.display = "block";
+}
