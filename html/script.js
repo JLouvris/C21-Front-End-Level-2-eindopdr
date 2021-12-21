c52398923a424c2dfc1af862329a4ef7d9b01e52
Jurgen Louvris <jurgen.louvris@student.rocva.nl>
2021-12-21 11:02:54 +0100

    Aanpassingen

--- html/script.js
+++ html/script.js
@@ -2,9 +2,11 @@ function Test() {
     document.getElementById("blok").style.display = "block";
 }
 
+
 function pic() {
     document.getElementById("pic").style.display = "block";
 }
+
 function code() {
     document.getElementById("code").style.display = "block";
 }
